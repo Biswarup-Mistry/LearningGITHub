9153dddd3c34639d6e4bce5a9cc8641718a9d21b
Biswarup-Mistry <biswarupmistry3@gmail.com>
2021-05-13 11:42:46 +0530

    Not so First Commit

---
+++ demo.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string name,surname;
+    cout<<"This is the Demo Program \nHello World ! "<<endl;
+    cout<<"Enter Your First Name : "<<endl;
+    cin>>name;
+    cout<<"Enter Your Last Name : "<<endl;
+    cin>>surname;
+    cout<<"Your Name is : "<<name<<" "<<surname<<endl;
+    //cout<<"Your Last Name is : "<<surname<<endl;
+    return(0);
+}
